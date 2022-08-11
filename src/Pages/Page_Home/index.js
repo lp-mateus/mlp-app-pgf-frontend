cbb4cc5665acc1530aed2de5f752a138286c3287
lp-mateus <81657778+lp-mateus@users.noreply.github.com>
2022-08-11 10:57:14 -0300

    fix: /page_home

--- src/Pages/Page_Home/index.js
+++ src/Pages/Page_Home/index.js
@@ -4,6 +4,7 @@ import { DefaultButton } from '../../Components/Button';
 
 // CSS
 import './style.css';
+import FigureSavingPig from '../../Images/ilu-savingmoney.jpg';
 
 export function PageHome() {
     // Metódos
@@ -11,13 +12,51 @@ export function PageHome() {
     return(
         <> 
             <Header />
-            <br></br>
+
             <section className="home-titulo">
-                <div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <div className="home-color">
                     <h1>Plataforma de Gerenciamento Financeiro</h1>
                 </div>
                 <DefaultButton text="Cadastrar"/>
             </section>
+
+            <br></br>
+
+            <section className="home-subtitulo">
+                <h3>Poupança</h3>
+                <hr></hr>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis at sem sed faucibus. Fusce fermentum molestie urna, et tincidunt tellus convallis viverra.
+                </p>
+                <img src={FigureSavingPig} alt="ilustração-poupando" className="ilu-style"></img>
+            </section>
+
+            <section className="home-subtitulo">
+                <h3>Poupança</h3>
+                <hr></hr>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis at sem sed faucibus. Fusce fermentum molestie urna, et tincidunt tellus convallis viverra.
+                </p>
+                <img src={FigureSavingPig} alt="ilustração-poupando" className="ilu-style"></img>
+            </section>
+
+            <section className="home-subtitulo">
+                <h3>Poupança</h3>
+                <hr></hr>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis at sem sed faucibus. Fusce fermentum molestie urna, et tincidunt tellus convallis viverra.
+                </p>
+                <img src={FigureSavingPig} alt="ilustração-poupando" className="ilu-style"></img>
+            </section>
+
+
+
+            <br></br>
+
+            
         </>
     )
 }
